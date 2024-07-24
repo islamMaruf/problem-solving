c0437724be991f8f5fbd57fada95c3c85a47e84f
maruf.islam <maruf.islam@miaki.co>
2024-07-24 23:57:47 +0600

    leetcode - #33 solved

---
+++ leetcode/problem-33.js
@@ -0,0 +1,79 @@
+/**
+ * 33. Search in Rotated Sorted Array
+Medium
+Topics
+Companies
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 5
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-104 <= target <= 104
+
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+let nums = [4, 5, 6, 7, 0, 1, 2];
+let target = 1;
+
+let search = function search(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[start] <= nums[mid]) {
+            if (nums[start] <= target && target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
+
+console.log(search(nums, target));
+
